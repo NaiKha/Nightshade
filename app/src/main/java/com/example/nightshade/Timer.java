0edc5f5507e057292d5f8ef25e6b63bc8383348f
= <=>
2025-03-14 12:35:49 +0100

    First design draft of the timer page

--- app/src/main/java/com/example/nightshade/Timer.java
+++ app/src/main/java/com/example/nightshade/Timer.java
@@ -11,24 +11,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Timer extends AppCompatActivity {
+    private boolean started = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_timer);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button timer = (Button)findViewById(R.id.timer);
 
-        timer.setOnClickListener(new View.OnClickListener() {
+        Button start_pause = (Button)findViewById(R.id.start_btn);
+
+        start_pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Timer.class));
+                started = !started;
+                if (started){
+                    start_pause.setText("Pause");
+                } else{
+                    start_pause.setText("Start");
+                }
             }
         });
     }
