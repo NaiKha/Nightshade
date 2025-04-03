bf3b9b8e231336cefe7e0dcf31cc31eaf8e31ec5
NaiKha <naimakh971@gmail.com>
2025-04-03 11:46:12 +0200

    fri-lek knappen i movement är nu kopplad till fri-lek sidan

--- app/src/main/java/com/example/nightshade/FreeTime.java
+++ app/src/main/java/com/example/nightshade/FreeTime.java
@@ -1,4 +1,25 @@
 package com.example.nightshade;
 
-public class FreeTime {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.ComponentActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FreeTime extends ComponentActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_free_time);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
