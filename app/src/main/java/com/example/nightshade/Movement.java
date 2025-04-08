251fdf6df95fb298ebe892a833b974067ec04374
NaiKha <naimakh971@gmail.com>
2025-04-08 16:01:01 +0200

    added functionality for shake for random activity

--- app/src/main/java/com/example/nightshade/Movement.java
+++ app/src/main/java/com/example/nightshade/Movement.java
@@ -1,9 +1,15 @@
 package com.example.nightshade;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.ComponentActivity;
 import androidx.activity.EdgeToEdge;
@@ -11,7 +17,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Movement extends ComponentActivity {
+import java.util.Random;
+
+public class Movement extends ComponentActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+
+    private float ax, ay, az;
+    private long lastShakeTime = 0;
+    private String[] answers = {"Take a deep breath and exhale into the microphone.",
+            "Wave your phone in the air like you just don't care!", "Up down turn around.",
+            "Take a walk!",
+            };
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -30,5 +51,85 @@ public class Movement extends ComponentActivity {
                 startActivity(new Intent(Movement.this, FreeTime.class));
             }
         });
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        // set up baseline
+        mAccel = 9f; // 10 approx. earth's gravity, was too high
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // fetch x, y, z values, first checking if the sensor triggered is the accelerometer
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            ax = event.values[0];
+            ay = event.values[1];
+            az = event.values[2];
+        }
+
+        // save previous acceleration
+        mAccelLast = mAccelCurrent;
+        // use total acceleration magnitude formula
+        mAccelCurrent = (float) Math.sqrt((double) ax * ax + ay * ay + az * az);
+        // difference between last and new acceleration
+        float delta = mAccelCurrent - mAccelLast;
+        // multiply by 0.9f for a more stable shake detection
+        mAccel = mAccel * 0.9f + delta;
+        //mAccel = delta;
+
+        if (mAccel > 10f) {
+            // add a time check for last shake to prevent answers being generated directly after each other.
+            long time = System.currentTimeMillis();
+            if (time - lastShakeTime > 2000){
+                lastShakeTime = time;
+                // generate answer
+                randomAnswer();
+
+                // vibrate
+                vibrate();
+
+                // add sound effect?
+            }
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    private void randomAnswer() {
+        Random r = new Random();
+        int index = r.nextInt(answers.length); // random nbr between 0 (incl.) and length of array (excl.)
+        String rAnswer = answers[index];
+        TextView showAnswer = (TextView) findViewById(R.id.textView3);
+        showAnswer.setText(rAnswer);
+    }
+
+    private void vibrate() {
+        // Get instance of Vibrator from current Context
+        Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        // Vibrate for 400 milliseconds
+        v.vibrate(400);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
