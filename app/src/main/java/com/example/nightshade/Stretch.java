53a97473523b87b5a7d83dbd37ac3a969f495b86
fiddelifahlborg <marta.fahlborg@gmail.com>
2025-05-15 15:29:04 +0200

    Stretch has confetti

--- app/src/main/java/com/example/nightshade/Stretch.java
+++ app/src/main/java/com/example/nightshade/Stretch.java
@@ -2,18 +2,32 @@ package com.example.nightshade;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.concurrent.TimeUnit;
+
+import nl.dionsegijn.konfetti.core.PartyFactory;
+import nl.dionsegijn.konfetti.core.emitter.Emitter;
+import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
+import nl.dionsegijn.konfetti.core.models.Shape;
+import nl.dionsegijn.konfetti.core.models.Size;
+import nl.dionsegijn.konfetti.xml.KonfettiView;
 
 public class Stretch extends AppCompatActivity implements SensorEventListener {
 
@@ -27,6 +41,8 @@ public class Stretch extends AppCompatActivity implements SensorEventListener {
     private boolean isReady = false;
     private long lastEventTime = 0;
     private long lastTimestamp = 0L;
+    private int counter = 0;
+    private KonfettiView konfettiView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +50,13 @@ public class Stretch extends AppCompatActivity implements SensorEventListener {
         setContentView(R.layout.activity_stretch);
 
         stretchImage = findViewById(R.id.stretchImage);
-        readyText = findViewById(R.id.readyText);
         stretchImage.setImageResource(R.drawable.stretch1);
+
+        readyText = findViewById(R.id.readyText);
         readyText.setVisibility(View.VISIBLE);
+
+        konfettiView = findViewById(R.id.konfettiView);
+
 
         ImageView back = findViewById(R.id.back);
         back.setOnClickListener(v -> didFinish());
@@ -75,13 +95,17 @@ public class Stretch extends AppCompatActivity implements SensorEventListener {
             float deltaTime = (nowNs - lastTimestamp) / 1_000_000_000f;
             float y = event.values[1];
             long now = System.currentTimeMillis();
-            if (Math.abs(y * deltaTime) > 0.275f && (now - lastEventTime) > 2000) {
+            if (Math.abs(y * deltaTime) > 0.250f && (now - lastEventTime) > 2000) {
                 toggleStretch();
                 lastEventTime = now;
                 resetTime();
             }
         }
         lastTimestamp = nowNs;
+
+        if (counter > 4) {
+            triggerCelebration();
+        }
     }
 
     @Override
@@ -92,11 +116,69 @@ public class Stretch extends AppCompatActivity implements SensorEventListener {
         stretchImage.setImageResource(armsUp ? R.drawable.stretch1 : R.drawable.stretch2);
         armsUp = !armsUp;
         giveFeedback();
+        counter += 1; 
     }
 
     private void giveFeedback() {
         Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(200);
+    }
+
+    private void triggerCelebration() {
+        EmitterConfig emitterConfig = new Emitter(500, TimeUnit.MILLISECONDS).max(500);
+        konfettiView.start(
+                new PartyFactory(emitterConfig)
+                        .shapes(Shape.Circle.INSTANCE, Shape.Square.INSTANCE)
+                        .spread(360)
+                        .position(0.0,0.0,1.0,1.0)
+                        .sizes(new Size(8,50,10))
+                        .timeToLive(2000).fadeOutEnabled(true).build()
+        );
+        vibrateAndNotify();
+        ConstraintLayout dialogLayout = findViewById(R.id.completeConstraintLayout);
+        View view  = LayoutInflater.from(Stretch.this).inflate(R.layout.movement_dialog, dialogLayout);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Stretch.this);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+
+        if (alertDialog.getWindow() != null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        sensorManager.unregisterListener(this);
+        alertDialog.show();
+        new Handler().postDelayed(() -> {
+            if (alertDialog.isShowing()) {
+                alertDialog.dismiss();
+                startActivity(new Intent(Stretch.this, Movement.class));
+            }
+        }, 5000);
+
+    }
+
+    private void vibrateAndNotify() {
+        try {
+            Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            if (v != null && v.hasVibrator()) {
+                v.vibrate(2000);
+            }
+
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.achievement);
+            if (mp != null) {
+                mp.setOnCompletionListener(mediaPlayer -> {
+                    try {
+                        mediaPlayer.stop();
+                        mediaPlayer.release();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                mp.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void resetTime() {
